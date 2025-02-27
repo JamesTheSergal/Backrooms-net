e3d41117467ed0bb7901c084ab7b48c7d4897c01
James Sergal <jamesthesergal@gmail.com>
2025-02-27 03:01:11 -0500

    Introduce PLV_VERSION

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,12 @@
 BR_VERSION = "0.0.1-alpha"
+PLV_VERSION = "0.0.1-alpha" # Project launch vehicle
 SUPPORTED_HANDSHAKES = ["0.0.1-alpha"]
 
-import addonSystem
+# Always logging first
+import core.Logging
+import core.addonSystem
+
+# Attempt to call for a global logger to get this show on the road
+core_logger = core.Logging.createNewLogger("node.log")
+core_logger.info(f"Backroom-net Core version: {BR_VERSION}")
+core_logger.info(f"Platform Launch Vehicle (PLV) version: {PLV_VERSION}")
