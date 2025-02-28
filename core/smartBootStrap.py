1ed98fc373f20992829fa044de6fba956474ae4d
James Sergal <jamesthesergal@gmail.com>
2025-02-28 02:32:31 -0500

    fixed it

--- core/smartBootStrap.py
+++ core/smartBootStrap.py
@@ -1,5 +1,6 @@
 from rich.console import Console
 from rich.spinner import Spinner
+import time
 
 import os
 
@@ -8,18 +9,19 @@ console = Console()
 
 def find_packages(directory):
     packages = []
-    spinner = Spinner("dots", text="Scanning modules...")
-    console.print(spinner)
-    for root, dirs, files in os.walk(directory):
-        if 'venv' in dirs:
-            dirs.remove('venv')
+    with console.status("Scanning modules...", spinner="dots") as status:
+        for root, dirs, files in os.walk(directory):
+            if 'venv' in dirs:
+                dirs.remove('venv')
+
+            if '__init__.py' in files:
+                # Remove the __init__.py from the path and append to the list
+                package_path = os.path.relpath(root, directory)
+                packages.append(package_path.replace(os.sep, '.'))
             
-        if '__init__.py' in files:
-            # Remove the __init__.py from the path and append to the list
-            package_path = os.path.relpath(root, directory)
-            spinner.update(text=f'Scanning modules... ({package_path})')
-            packages.append(package_path.replace(os.sep, '.'))
-    return packages
+            status.update(status=f'Scanning modules... ({root})')
+            time.sleep(0.15)
+        return packages
 
 def bootStrap():
     current_directory = os.getcwd()
