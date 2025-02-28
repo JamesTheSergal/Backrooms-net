8884ff30e641bc735645013cb63637876428e98e
James Sergal <jamesthesergal@gmail.com>
2025-02-27 19:05:30 -0500

    Not required anymore due to PLV package

--- core/addonSystem/baseAddon/addonheaders.py
+++
@@ -1,9 +0,0 @@
-import dataclasses
-
-@dataclasses.dataclass(frozen=True, order=True)
-class BaseAddonHeader:
-    addonName = str
-    addonVersion = str
-    author = str
-    website = str
-    internalModule = bool
