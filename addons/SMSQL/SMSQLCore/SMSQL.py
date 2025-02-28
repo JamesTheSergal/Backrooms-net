04dcec4ab14c93c1130a57711ad1dd9adfc83be6
James Sergal <jamesthesergal@gmail.com>
2025-02-27 19:03:02 -0500

    Staged SMSQL Addon

---
+++ addons/SMSQL/SMSQLCore/SMSQL.py
@@ -0,0 +1,3 @@
+# "Sergal Miniaturized SQL"
+# Not the best name in the world, but this thing sure does help me crank out some SQL.
+
---
+++ addons/SMSQL/__init__.py
@@ -0,0 +1,7 @@
+import core.PLV.AddonSystem.Addon.v1 as PackageVersion
+from core.PLV.AddonSystem.Addon.v1 import AddonDefaultSettings, properties
+
+class SMSQLCore:
+    
+    def __init__(self):
+        pass
