49cc432c00d4e753476720ffc007eab3d25fa8b1
James Sergal <jamesthesergal@gmail.com>
2025-06-29 15:45:34 -0400

    New brSockets module

---
+++ brCore/brSockets/__init__.py
@@ -0,0 +1,9 @@
+from .. import BR_VERSION
+from .. import brNodeHsLog
+from .. import brAgentLog
+
+# Import protocols
+import brCore.brSockets.brProtocols
+
+import brCore.brSockets.brPacket
+from brCore.brSockets.brNodeAgent import brSocketAgent
