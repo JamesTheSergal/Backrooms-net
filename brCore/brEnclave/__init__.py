48597937f4c24c1645ce9af50f7d46c93801dbef
James Sergal <jamesthesergal@gmail.com>
2025-07-19 00:41:36 -0400

    Remove old logging code

--- brCore/brEnclave/__init__.py
+++ brCore/brEnclave/__init__.py
@@ -1,20 +1,2 @@
-import logging
-import os
-from pathlib import Path
-
-import Enclave
-
-
-brEnclFormat = logging.Formatter(
-        "{asctime}.{msecs} - [{thread} {module}.{funcName}:({lineno})] - [{levelname}] - {message}",
-        style="{",
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-logger = logging.getLogger("brSecurity")
-logger.propagate = False
-logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler(f'temp/brSecurity.log', mode='a')
-console_handler = logging.StreamHandler()
-file_handler.setFormatter(brEnclFormat)
-console_handler.setFormatter(brEnclFormat)
-logger.addHandler(file_handler)
+from brCore.brEnclave import notrustvars
+from brCore import brEnclaveLog
