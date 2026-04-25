daaa44061887c5771bd9770d2fb4f4c652c491ba
James Sergal <jamesthesergal@gmail.com>
2026-04-24 21:57:13 -0400

    Re-implement scheduled tasks with library

--- brCore/brNodeNet/brController.py
+++ brCore/brNodeNet/brController.py
@@ -10,6 +10,8 @@ import threading
 import time
 import uuid
 import requests
+import schedule
+from schedule import every, repeat, run_pending
 from ..upnphelper import configureUPNP, removeUPNP
 from . import brNodeCoreLog
 from brCore.brSockets.brPacket import brPacket
@@ -20,11 +22,12 @@ from .brRoute import brRoute
 from ..brSockets.brHandshake import brBasicHandshake
 from .router import Router
 from .events import EventType, NetworkEvent, DHTRequest, EndPointEvent
-from .brDHT import brDHT
+from .brDHT import brDHT, brDHTQueryHelper
 from ..brSockets.netconnection import netconnection
 from .brEndpoint import brEndpoint
 from .controllertasks import ControllerTask, TaskType
 from queue import Queue
+import json
 
 BR_VERSION = "0.0.1-alpha"
 
@@ -38,14 +41,16 @@ class brNodeServer:
 
     def __init__(self, secureEnclave:Enclave, dhtServer:brDHT, webport:int) -> None:
         
+        # Settings
         self.node_port = random.randrange(13000, 14000) # For testing
         self.web_port = webport
+        self.max_control_routes:int = 10
 
         self.secureEnclave = secureEnclave
         self.dht = dhtServer
+        self.dht_query = brDHTQueryHelper(dhtServer, secureEnclave)
         self.event_queue = Queue(maxsize=10000)
         self.dht_queue = Queue(maxsize=10000)
-        self.controller_tasks = Queue(maxsize=10000)
         self.connection_manager = ConnectionManager(secureEnclave, self.event_queue)
         
         self.knownNodes:list[brNode] = []
@@ -66,6 +71,7 @@ class brNodeServer:
         #if result is not False:
         #    self.externalIP = result
         #    logger.info("UPNP configured for Node Server.")
+        self.externalIP = "127.0.0.1"
             
         
         self.connection_manager.startListener("0.0.0.0", self.node_port)
@@ -103,15 +109,20 @@ class brNodeServer:
                 self._debug_event(event) # Just comment out when not needed
                 self._handle_event(event)
                 self.total_events += 1
+                continue
             except Empty:
-                self._do_scheduled_tasks()
+                pass
+            
+            schedule.run_pending()
                 
     
     def _initial_persistant_task_setup(self):
-        self.controller_tasks.put(ControllerTask.create_persistent_task(TaskType.EVAL_CONTROL_CONS, 300))
-        self.controller_tasks.put(ControllerTask.create_persistent_task(TaskType.READ_NEWS, 60))
-        self.controller_tasks.put(ControllerTask.create_persistent_task(TaskType.MAINTAIN_ENC_RECORD, 300))
-        self.controller_tasks.put(ControllerTask.create_persistent_task(TaskType.LOCAL_DHT_TTL_CLEANUP, 300))
+        schedule.every(5).minutes.do(brNodeServer._local_dht_ttl_cleanup, self)
+        schedule.every(5).minutes.do(brNodeServer._maintain_enc_record, self)
+        schedule.every(5).minutes.do(brNodeServer._maintain_dht, self)
+        schedule.every().minute.do(brNodeServer._eval_control_cons, self)
+        schedule.every(30).seconds.do(brNodeServer._controller_read_news, self)
+        
     
     def _prestart(self):
         # Restore the UUID so that other nodes know who we are
@@ -123,7 +134,7 @@ class brNodeServer:
             self.uuid = self.secureEnclave.returnData("selfUUID")
             
         # Set basic handshake info object
-        self.config = {"uuid": self.uuid, "dhtport": self.dht.serverport, "webport": self.web_port}
+        self.config = {"uuid": self.uuid, "dhtport": self.dht.serverport, "webport": self.web_port, "nodeport": self.node_port}
         self.router.config = self.config
     
     def _handle_event(self, event: NetworkEvent):
@@ -151,18 +162,18 @@ class brNodeServer:
     def _handle_new_connection(self, route: brRoute):
         # Decide if we need to run handshake
         if route.connectionType == brRoute.brConnectionDirection.INITIATED:
+            route.setDestinations(destination=f'{route.externalNode.localNodeID} (unconfirmed)', origin=self.uuid)
             first_hello = brPacket()
             first_hello.setMessageType(brPacket.brMessageType.INTRODUCE)
             self.router._send_to_route(route, first_hello)
-            route.connectingTo = f'{route.externalNode.localNodeID} (Unconfirmed)'
-            route.connectingFrom = self.uuid
         else:
-            route.connectingFrom = self.uuid
-            route.connectingTo = f'{route.externalNode.localNodeID} (Unconfirmed)'
+            route.setDestinations(destination=self.uuid, origin=f'{route.externalNode.localNodeID} (unconfirmed)')
     
     def _submit_known_node(self, route:brRoute):
         self.knownNodes.append(route.externalNode)
-        self.router._perform_route_upgrade(route)
+        if len(self.knownNodes) == 0:
+            self.router._perform_route_upgrade(route)
+            logger.info("No other nodes connected, automatically attempting to upgrade route to CONTROL.")
         if len(self.knownNodes) > 0 and len(self.dht.dhtServer.bootstrappable_neighbors()) == 0:
             self.dht.setBootstrapList([(route.externalNode.nodeIP, route.externalNode.dhtport)])
             logging.info("Just bootstrapped the DHT network.")
@@ -186,38 +197,6 @@ class brNodeServer:
 
     # Tasks
     # 
-    def _do_scheduled_tasks(self):
-        started = time.time()
-        end = started + 1 # Yes, for one second we can do tasks.
-        
-        
-        while not self.shutdown and (end < time.time()):
-            try:
-                task:ControllerTask = self.controller_tasks.get(timeout=0.3)
-                if task.persistent:
-                    self._handle_task(task)
-                    task.last_executed = time.time()
-                    self.controller_tasks.put(task)
-                    continue
-                
-                if task.isexetime():
-                    self._handle_task(task)
-                    
-            except Empty:
-                break
-    
-    def _handle_task(self, task: ControllerTask):
-        if task.task_type == TaskType.LOCAL_DHT_TTL_CLEANUP:
-            self._local_dht_ttl_cleanup()
-        elif task.task_type == TaskType.MAINTAIN_ENC_RECORD:
-            self._maintain_enc_record(task)
-        elif task.task_type == TaskType.MAINTAIN_DHT:
-            self._maintain_dht(task)
-        elif task.task_type == TaskType.EVAL_CONTROL_CONS:
-            self._eval_control_cons(task)
-        elif task.task_type == TaskType.READ_NEWS:
-            self._controller_read_news(task)
-    
     def _local_dht_ttl_cleanup(self):
         removed = 0
         for key in self.dhtResponses.keys():
@@ -228,176 +207,57 @@ class brNodeServer:
         if removed > 0:
             logger.info(f'Removed {removed} DHT Responses that expired.')
     
-    def _maintain_enc_record(self, task:ControllerTask):
+    def _maintain_enc_record(self):
         #if self.secureEnclave.isEncKey("knownNodes"):
         #    self.secureEnclave.returnData("knownNodes")
         #else:
         #    logger.info("Bootstrapping the local node with previously known nodes.")
         logger.info("Maintain Enclave Task ran successfully.")
     
-    def _maintain_dht(self, task:ControllerTask):
-        logger.info("Maintain DHT Task ran successfully.")
+    def _maintain_dht(self):
+        if not self.dht.hasBootStrapped:
+            logger.error("Maintain DHT task failed because we are not bootstrapped yet.")
+            
+        # Refresh node info
+        self.dht_query.publish(self.uuid, "node")
+        self.dht_query.publish(self.uuid, self.secureEnclave.assignedIdentity.publicKey.save_pkcs1(), "pubkey")
+        self.dht_query.publish(self.uuid, self.web_port, "webport")
+        self.dht_query.publish(self.uuid, self.node_port, "nodeport")
+        self.dht_query.publish(self.uuid, self.dht.serverport, "dhtport")
+        self.dht_query.publish(self.uuid, self.externalIP, "address")
         
-    def _maintain_dht(self, task:ControllerTask):
-        logger.info("Maintain DHT Task ran successfully.")
+        # Refresh endpoint info
+        for endpoint_key in self.router.active_endpoints.keys():
+            endpoint:brEndpoint = self.router.active_endpoints[endpoint_key]
+            self.dht_query.publish(endpoint.endpoint_uuid, "endpoint")
+            self.dht_query.publish(endpoint.endpoint_uuid, self.uuid, typestr="endpoint")
+            
+        known = []
+        for node in self.knownNodes:
+            known.append(node.localNodeID) 
+        self.dht_query.publish(self.uuid, json.dumps(known), typestr="knownnodes")
+        
+        for route in self.router.active_routes:
+            self.dht_query.publish(route.routeID, "route")
+            self.dht_query.publish(route.routeID, route.connectingFrom, "origin")
+            self.dht_query.publish(route.routeID, route.connectingTo, "destination")
+            self.dht_query.publish(route.routeID, route.routeType.name, "type")
+            
     
-    def _eval_control_cons(self, task:ControllerTask):
-        logger.info("Maintain DHT Task ran successfully.")
+    def _eval_control_cons(self):
         
-    def _controller_read_news(self, task:ControllerTask):
+        # Evaluate test routes
+        
+        control_routes_active = self.router.fetch_all_control_routes()
+        
+        logger.info(f'Controller has {control_routes_active} active controls')
+        
+        #for route in self.router.fetch_all_test_routes():
+        #    route:brRoute
+        #    if control_routes_active < self.max_control_routes:
+        #        logger.info("TEST: C")
+    
+    def _controller_read_news(self):
         all_control_routes = self.router.fetch_all_control_routes()
         logger.info(f"Controller has {len(all_control_routes)} control routes.")
-        
-    
-                    
-    def __routerOld__(self):
-        if packet.messageType == brPacket.brMessageType.INTRODUCE:
-
-            if nodeRoute.thirdParty.identity == None:
-                thirdpartyconfig = packet.data.decode('utf-8')
-                configSplit = thirdpartyconfig.split('-')
-                thirdPartyWebPort = int(configSplit[0])
-                thirdPartyNodePort = int(configSplit[1])
-
-                nodeRoute.thirdParty.webPort = thirdPartyWebPort
-                nodeRoute.thirdParty.nodePort = thirdPartyNodePort
-                if not nodeRoute.thirdPartyPubKeyCheck():
-                    logger.error("Could not get public key from node to establish identity!")
-                    return False
-                
-            chunks = nodeRoute.thirdParty.identity.chunkEncrypt(str(nodeRoute.routeSecret).encode('utf-8')) # Should just be one chunk
-            reply = brPacket()
-            reply.setMessageType(brPacket.brMessageType.CHALLENGE)
-            reply.setMessageVersion(BR_VERSION)
-            reply.data = chunks[0]
-            return reply.buildPacket()
-        elif packet.messageType == brPacket.brMessageType.CHALLENGE_RES:
-            try:
-                result = self.secureEnclave.assignedIdentity.decryptChunk(packet.data)
-            except:
-                logger.warning(f'Challenge failed against node at {nodeRoute.thirdParty.nodeIP} - possible security breach', exc_info=True)
-                return False
-            
-            try:
-                check = int(result)
-            except:
-                logger.warning(f'Challenge failed against node at {nodeRoute.thirdParty.nodeIP} - bad data - Possible attack')
-                return False
-            
-            if check == nodeRoute.routeSecret:
-                nodeRoute.setHandShakeComplete()
-                if nodeRoute.routeType == nodeRoute.brRouteType.TEST:
-                    nodeRoute.upgradeRouteType(brRoute.brRouteType.CONTROL)
-                    with self.thrLock:
-                        self.inTesting.remove(nodeRoute)
-                        self.controlRoutes.append(nodeRoute)
-                reply = brPacket()
-                reply.setMessageType(brPacket.brMessageType.ENCR_COMMS)
-                reply.setMessageVersion(BR_VERSION)
-                return reply.buildPacket()
-            else:
-                logger.warning(f'Response to our challenge was invalid! Their response: {check}')
-                return False
-        elif packet.messageType == brPacket.brMessageType.CHALLENGE:
-            try:
-                result = self.secureEnclave.assignedIdentity.decryptChunk(packet.data)
-            except:
-                logger.warning(f"Failed to decrypt challenge from third party node at {nodeRoute.thirdParty.nodeIP} - possible attack", exc_info=True)
-                return False
-            sendback = nodeRoute.thirdParty.identity.chunkEncrypt(result)
-            reply = brPacket()
-            reply.setMessageType(brPacket.brMessageType.CHALLENGE_RES)
-            reply.setMessageVersion(BR_VERSION)
-            reply.data = sendback[0]
-            return reply.buildPacket()
-        elif packet.messageType == brPacket.brMessageType.ENCR_COMMS:
-            nodeRoute.setHandShakeComplete()
-            nodeRoute.encryptionUpgraded = True
-            return True
-        elif packet.messageType == brPacket.brMessageType.CALLBACK_PING:
-            return True
- 
-    def __networkController__(self):
-        logger.info("Network controller thread started.")
-
-        # Quickly see if we have saved ourselves a UUID + add stuff to DHT
-        if not self.secureEnclave.isEncKey("selfUUID"):
-            self.uuid = uuid.uuid4()
-            logger.info(f"Network controller new UUID is: {self.uuid}")
-            self.secureEnclave.insertData("selfUUID", self.uuid)
-            self.secureEnclave.insertData("webPort", self.webPort)
-        else:
-            self.uuid = self.secureEnclave.returnData("selfUUID")
-        
-        # Values that need to be updated every startup
-        self.secureEnclave.updateEntry("webPort", self.webPort)
-        self.secureEnclave.updateEntry("dhtPort", self.dht.serverport)
-        
-        if self.dht.hasBootStrapped:
-            self.dht.setRequest(f'{self.uuid}_pubkey', self.secureEnclave.assignedIdentity.publicKey.save_pkcs1())
-            self.dht.setRequest(f'{self.uuid}_nodeport', self.nodePort)
-        else:
-            logger.error("DHT is not bootstrapped yet. Cannot publish our public key or node port yet.")
-        
-        # Rebuild and startup procedure
-        # 
-        #
-        if self.secureEnclave.isEncKey("knownNodes"):
-            routes = self.generateRoutesFromEnclaveSave()
-            if routes is not None:
-                for pendingRoute in routes:
-                    self.socketControl.connectRequest.put(pendingRoute)
-                logger.info(f'Finished adding {len(routes)} routes from the Enclave to reconnect to...')
-            
-        else:
-            if Path('seedservers.txt').is_file():
-                routes = self.generateRoutesFromSeedFile('seedservers.txt')
-                for pendingRoute in routes:
-                    self.socketControl.connectRequest.put(pendingRoute)
-                logger.info(f'Finished adding {len(routes)} routes from the Enclave to reconnect to...')
-
-        
-        logger.info("Network controller ready.")
-
-        # Main Controller Loop
-        #
-        #
-
-        while not self.shutdown:
-
-            looptime = time.time()
-            
-            if self.secureEnclave.isEncKey("connect_ip"):
-                logger.info("Got connection request from the web server")
-                newNodeObject = brNode(nodeIP=self.secureEnclave.returnData("connect_ip"), nodePort=self.secureEnclave.returnData("connect_port"))
-                self.secureEnclave.deleteKey("connect_ip")
-                self.secureEnclave.deleteKey("connect_port")
-                
-                pendingRoute = brRoute(routeType=brRoute.brRouteType.TEST, externalNode=newNodeObject, connectionType=brRoute.brConnectionDirection.INITIATED)
-                self.socketControl.connectRequest.put(pendingRoute)
-                
-            if self.dht.hasBootStrapped is False:
-                bootstraplist = []
-                for node in self.knownNodes:
-                    if node.dhtport != 0:
-                        bootstraplist.append((node.nodeIP,node.dhtport))
-                if len(bootstraplist) > 0:
-                    self.dht.setBootstrapList(bootstraplist)
-            
-            time.sleep(1)
-        
-        # Broke out, begin shutting down and saving node/route states.
-        #logger.info("Controller is waiting for all other threads to shut down before saving...")
-        #while self.inboundThread.is_alive() and self.outboundThread.is_alive():
-            #time.sleep(0.2)
-        
-        #TODO: at a later date, make routes restorable
-        logger.info("Saving node data - Gathering nodes...")
-        for node in self.knownNodes:
-            node.setNodeDisconnectedState()
-  
-        self.secureEnclave.updateEntry("knownNodes", self.knownNodes, True)
-            
-
-
 
